d6e5919586cf83fa24f95357244538572d7fd5d5
Alexandr <blix@list.ru>
2025-02-28 21:34:23 +0300

    Плэйсхолдеры на главной странице

--- config/urls.py
+++ config/urls.py
@@ -21,10 +21,11 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView
 
 from config import settings
+from users.forms import UserAuthForm
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("", LoginView.as_view(template_name="users/login.html", form_class=UserAuthForm), name="login"),
     path("users/", include("users.urls", namespace="users")),
     path("diary/", include("diary.urls", namespace="diary")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
